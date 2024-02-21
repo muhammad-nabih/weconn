5427aa89fcb630426a07dab9344fde9182dfc482
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-02-21 14:44:47 +0200

    fix bug

--- src/app/(dynamicPages)/articles/page.js
+++ src/app/(dynamicPages)/articles/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./Articles.module.css";
 import ArticleCard from "@/components/articleCard/ArticleCard";
-import { PinContainer } from "@/ui/3d-pin";
+
 const Articles = async () => {
   const response = await fetch(`https://dummyjson.com/products`);
   const data = await response.json();
@@ -11,14 +11,8 @@ const Articles = async () => {
     <section className={styles.articlesContainer}>
       <h2 className={styles.h2}>Articles</h2>
       <div className={styles.container}>
-        {productsData.map((product) => (
-          <PinContainer
-            title={product.title}
-            href={`https://dummyjson.com/products/${product.id}`}
-            key={product.id}
-          >
-            <ArticleCard product={product} />
-          </PinContainer>
+        {productsData.map((product, index) => (
+          <ArticleCard key={index} product={product} />
         ))}
       </div>
     </section>
